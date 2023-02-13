7c8cfe6eabcb1ad05b1ca6547814183beda7ad18
Denis Senchishen <dsenchishen@icloud.com>
2023-02-13 10:42:40 +0200

    fix: fix None in title breaking file name on render

--- puml_generator/management/commands/utils/utils.py
+++ puml_generator/management/commands/utils/utils.py
@@ -312,7 +312,7 @@ class PlantUml:
         """
         global_choices = dict()
 
-        uml = f"@startuml {self.title}\n"
+        uml = f"@startuml {self.title or ''}\n"
 
         if self.title:
             uml += f"""
